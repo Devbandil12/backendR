14dda0f9c70e07e399695bb511b93d361f850139
omkarkaurav <omkarkaurav41@gmail.com>
2025-08-04 17:31:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -39,8 +39,9 @@ razorpayWebhookHandler);
 
 
 // ───── JSON Body Parser (for all other routes) ─────
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ extended: false, limit: '5mb' }));
+
 
 // ───── Routes ─────
 app.use('/api/payments', paymentRoutes);
