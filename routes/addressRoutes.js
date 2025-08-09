b072c4e3ae476eea9d8117bf4f5a92341b4d15d3
omkarkaurav <omkarkaurav41@gmail.com>
2025-08-09 22:48:04 +0530

    Update addressRoutes.js

--- routes/addressRoutes.js
+++ routes/addressRoutes.js
@@ -1,10 +1,18 @@
-// src/routes/addressRoutes.js
 import express from "express";
-import { saveAddress, deleteAddress } from "../controllers/addressController.js";
+import {
+  saveAddress,
+  updateAddress,
+  listAddresses,
+  softDeleteAddress,
+  setDefaultAddress
+} from "../controllers/addressController.js";
 
 const router = express.Router();
 
 router.post("/save", saveAddress);
-router.post("/delete", deleteAddress);
+router.put("/update/:id", updateAddress);
+router.get("/list/:userId", listAddresses);
+router.delete("/soft-delete/:id", softDeleteAddress);
+router.put("/set-default/:id", setDefaultAddress);
 
 export default router;
